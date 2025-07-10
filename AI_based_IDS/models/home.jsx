fb6194934ed2c62da738ae31fbcfe9a9f68597d2
Lindo Dlamini <lindokuhledlams@icloud.com>
2025-07-11 00:20:46 +0200

    stuff

---
+++ AI_based_IDS/models/home.jsx
@@ -0,0 +1,19 @@
+import react from "react"
+import {Link} from "react-router-dom"
+
+export default function Home(){
+
+    return(
+        <html>
+<head>
+    {/* <meta charset="UTF-8" /> */}
+    <title>Welcome to my IDS powered by ML :P </title>
+</head>
+<body>
+    
+</body>
+</html>
+    )
+
+}
+
